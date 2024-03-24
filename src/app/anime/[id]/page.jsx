62b83f1ec00ef758230fb896967388ff97fd5bcb
rakategar <tegarrakaw@gmail.com>
2024-03-24 15:19:33 +0700

    Changes to be committed: 	modified:   src/app/anime/[id]/page.jsx 	modified:   src/app/users/dashboard/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -14,6 +14,8 @@ const Page = async({params : {id}}) => {
     const collection = await prisma.collection.findFirst({
         where: { user_email: user?.email, anime_mal_id: id }
     })
+    const vip = await prisma.vip.findFirst({where : {user_name : user.name}})
+    const showCommentBox = !!vip?.user_name; // Jika vip.user_name ditemukan, nilai akan menjadi true
 
     return (
         <>
@@ -56,7 +58,7 @@ const Page = async({params : {id}}) => {
             <div className="p-4">
                 <h3 className="text-2xl text-palete-primary mb-2 text-bold">Komentar Penonton</h3>
                 <CommentBox anime_mal_id={id}/>
-                {user && <CommentInput anime_mal_id={id} user_email={user.email} user_name={user.name} anime_title={detailAnime.data.title}/>}
+                {showCommentBox && <CommentInput anime_mal_id={id} user_email={user.email} user_name={user.name} anime_title={detailAnime.data.title}/>}
                 
             </div>
             <div>
--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -3,18 +3,23 @@ import Image from "next/image"
 import Link from "next/link"
 const Page = async() => {
     const user = await authUserSession()
+    const vip = await prisma.vip.findFirst({where : {user_name : user.name}})
+    const showCommentFiture = !!vip?.user_name; // Jika vip.user_name ditemukan, nilai akan menjadi true
+    console.log(vip);
     return (
         <div className="mt-8 text-palete-primary flex flex-col justify-center items-center">
             <h5 className="text-2xl font-bold py-4">Haii {user?.name} ...</h5>
+            <h5 className="text-palete-accent text-2xl font-bold py-4">{vip?.status}</h5>
             <Image src={user?.image} alt="..." width={250} height={250}></Image>
             <div className="py-8 flex gap-4 flex-warp justify-center items-center">
                 <Link href="/users/dashboard/collection" 
                 className="bg-palete-accent text-palete-dark font-bold px-4 py-2 text-xl rounded "
                 >My Colection</Link>
-
-                <Link href="/users/dashboard/comment" 
-                className="bg-palete-accent text-palete-dark font-bold px-4 py-2 text-xl rounded"
-                >My Comment</Link>
+                {
+                    showCommentFiture && <Link href="/users/dashboard/comment" 
+                    className="bg-palete-accent text-palete-dark font-bold px-4 py-2 text-xl rounded"
+                    >My Comment</Link>
+                }
             </div>
             <div>
                 <Link href="/users/dashboard/vip" 
